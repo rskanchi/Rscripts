abeec15b56b137a9e42c458b97dfb49e839bf44c
Rupa Kanchi <RupaSKanchi@gmail.com>
2023-07-18 05:10:17 -0500

    minor edits

--- R/readRscripts.R
+++ R/readRscripts.R
@@ -3,6 +3,5 @@
 readRscripts <- function(path){
   rFiles <- list.files(path = path, pattern = "*.R")
   sapply(rFiles, FUN = function(rF) source(paste(path, rF, sep = "/")))
-  
   message(paste("R scripts sourced from the folder", path))
 }
