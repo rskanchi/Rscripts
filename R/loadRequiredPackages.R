d1abc63aa31a24d5c91b69a02a49fa9a5b1205d9
Rupa Kanchi <RupaSKanchi@gmail.com>
2023-10-03 22:17:33 -0500

    added check for and installation of biocmanager packages

--- R/loadRequiredPackages.R
+++ R/loadRequiredPackages.R
@@ -4,7 +4,19 @@
 loadRequiredPackages <- function(required.packages){
   # what packages are not installed? 
   packages.not.installed <- required.packages[!(required.packages %in% installed.packages()[,"Package"])]
+  
+  
   # install if any not installed already
-  if(length(packages.not.installed)>0) {install.packages(packages.not.installed, repos = "http://cran.us.r-project.org")}
+  if(length(packages.not.installed) > 0) {
+    # if installation through biocManager
+    biocPackages <- packages.not.installed[packages.not.installed %in% BiocManager::available()]
+    if (length(biocPackages) > 0) {
+      BiocManager::install(biocPackages)
+      packages.not.installed <- setdiff(packages.not.installed, biocPackages)
+    } # end of if in length(biocPackages) > 0
+    
+    # after installing biocPackages, if any
+    install.packages(packages.not.installed, repos = "http://cran.us.r-project.org")
+    } # end of if in length(packages.not.installed)>0
   lapply(required.packages, require, character.only = TRUE)
 } # end of function loadRequiredPackages
