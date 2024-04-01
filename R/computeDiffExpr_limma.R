27900625b6d7defe4c48b1ee0d5072d9e88874b1
Rupa Kanchi <RupaSKanchi@gmail.com>
2024-04-01 14:05:55 -0500

    added the getviolin function to theis file

--- R/computeDiffExpr_limma.R
+++ R/computeDiffExpr_limma.R
@@ -91,6 +91,47 @@ computeDiffExpr_limma <- function(d, group_var, group_names, # d = expr data + t
     return(list(volcano = volcano_plot, result = res, diffexpr = diffexpr))
     
   } else return(list(result = res, diffexpr = diffexpr))
-  
-  
 } # end of computeDiffExpr
+
+
+# function to get box and violin plots for the specified continuous variables in a data set
+# data with sample rows and columns including xNames and yNames
+# specify the names of continuous traits in the argument yNames: yNames = colnames()[c()] or yNames = c("","","")
+
+getViolin_diffExprLimma <- function(d, res, xName, folder = NULL,
+                                    theme_size = 20, text_size = 10){
+  library(ggplot2)
+  library(gridExtra)
+  library(scales) # scientific()
+  if (is.null(folder)) folder <- "output/diffExpr_Limma_Violins"
+  createDir(folder = folder)
+  
+  #yNames, 
+  
+  for (x in xNames){
+    dfAOV <- lapply(yNames, FUN = function(y){
+      aov(data[, y] ~ data[, x] , data = data)
+    }) # end of sapply in y
+    names(dfAOV) <- yNames
+    
+    box_plot <- lapply(yNames, FUN = function(y){
+      aov.p <- summary(dfAOV[[y]])[[1]][1,5]
+      tbl.n <- table(data[, x])
+      ggplot(data, aes(x = factor(data[, x]), y=data[,y])) +
+        geom_violin(trim = TRUE, width = 0.3, size = 1.5) +
+        geom_boxplot(width=0.1, color="grey", alpha=0.1) +
+        stat_summary(fun = "mean",
+                     geom = "point",
+                     color = "red") +
+        xlab(x) + ylab(y) +
+        labs(caption = paste(paste("AOV p-value: ", scientific(aov.p), sep = ""),
+                             paste(names(tbl.n), tbl.n, sep = " = ", collapse = ", "), sep = "\n")) +
+        #labs(caption = paste("AOV p-value: ", scientific(aov.p), sep = "")) +
+        theme_bw(theme_size)
+    }) # end of lapply in y
+    
+    pdf(paste(folder, paste0("AOV_Boxplots_", x, ".pdf"), sep = "/"), height = 8, width = 8)
+    print(box_plot)
+    dev.off()
+  } # end of for loop in x
+} # end of function getAOVBoxplot
