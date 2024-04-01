d2ed40195e7733f59e3a9be81a5288a701d6c4ca
Rupa Kanchi <RupaSKanchi@gmail.com>
2024-04-01 13:31:28 -0500

    action when folder = NULL

--- R/createDir.R
+++ R/createDir.R
@@ -1,5 +1,6 @@
 # function to create a folder recursively, if it doesn't exist
 
-createDir <- function(folder) {
+createDir <- function(folder = NULL) {
+  if(is.null(folder)) dir.create("output") else
   if (!dir.exists(paste(folder))){dir.create(paste(folder), recursive = TRUE)}       
 }
